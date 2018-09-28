e427410e9eadd09e7e0d0b2dab0a47bf7d3791db
chuang] <33791604+chuang861012@users.noreply.github.com>
2018-09-29 00:43:45 +0800

    update .json

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ const config = {
             {from:"./src/events.js",to:"./"}
         ]),
         new ZipPlugin({
-            filename: "readAfter.zip"
+            filename: "readlater.zip"
         })
     ]
 }
